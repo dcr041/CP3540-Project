838d9a6ab4210ba09f7f13882ef02b403c4e84d0
mohamed mahmoud <93497410+mohamed-baioumy@users.noreply.github.com>
2022-06-15 20:42:59 -0300

    add timer for 24 hours  mohamed -work-place

--- front-end/src/Statistics.js
+++ front-end/src/Statistics.js
@@ -1,10 +1,36 @@
+import { useState} from 'react';
+
 export default function Statistics({ countCorrectAnswers }) {
+    const [hours, setHours]= useState(0);
+    const [min, setMin]= useState(0);
+    const [sec, setSecond]= useState(0);
+
+
+    setInterval(function time(){
+        var d = new Date();
+       var hours = 24 - d.getHours()-1;
+       var min = 60 - d.getMinutes();
+        if((min + '').length === 1){
+          min = '0' + min;
+        }
+       var sec = 60 - d.getSeconds();
+        if((sec + '').length === 1){
+              sec = '0' + sec;
+        }
+        setHours(hours)
+        setMin(min)
+        setSecond(sec)
+      }, 1000);
     return (
         <>
             <p>
                 You got {countCorrectAnswers} correct!
             </p>
             <p>Thank you for playing!</p>
+
+
+            <p> hours ={hours} minutes={min} second={sec}</p>
+
         </>
     );
 }
