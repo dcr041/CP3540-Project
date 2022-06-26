1947bd7daa11a4e65d9d9c8a64e0728395411ad7
dcr041 <95144365+dcr041@users.noreply.github.com>
2022-06-26 15:35:31 -0700

    commit on 'Work-in-Progress-Daniel'

--- front-end/src/NavBar.js
+++ front-end/src/NavBar.js
@@ -2,17 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {faChartSimple} from '@fortawesome/free-solid-svg-icons';
+import {faChartSimple, faHome} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavBar = () => (
-    
     <>
         <Row className="bg-dark">
-            <Col className="text-start bg-dark text-white">
+            <Col className="text-start bg-dark text-white ms-1 mt-2">
+                <button className="btn">
+                    <Link to="/"><FontAwesomeIcon icon={faHome} size="2x"  /></Link>
+                </button>
+            </Col>  
+            <Col className="text-center bg-dark text-white">
                 <h2 className=" fw-bold fs-1 ms-3 mt-2">Quizarama</h2>
             </Col>
-            <Col className="text-end bg-dark text-white me-4 mt-2">
+            <Col className="text-end bg-dark text-white me-1 mt-2">
                 <button className="btn">
                     <Link to="/Statistics"><FontAwesomeIcon icon={faChartSimple} size="2x"  /></Link>
                 </button>
